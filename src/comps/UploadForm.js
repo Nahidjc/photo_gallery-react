032f1c8751fe3820a37c36941073dea0d28e521c
Nahid2194 <nahidjc80@gmail.com>
2021-03-19 22:58:34 +0600

    useStorage

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,14 +1,30 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+    const types = ['image/png', 'image/jpeg'];
     const handleUpload = (e) => {
         let selected = e.target.files[0];
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError("Please select an image file (png or jpeg)");
+        }
         console.log(selected);
     }
     return (
         <div>
             <form action="">
                 <input type="file" onChange={handleUpload} />
+
+                <div className="output">
+                    {
+                        error && <div className="error">{error}</div>
+                    }
+                    {file && <div className="file">{file.name}</div>}
+                </div>
             </form>
         </div>
     );
